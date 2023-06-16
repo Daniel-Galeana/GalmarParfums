11e5237e188bff8b76d8f656bd9424cd3896f1c2
Daniel Galeana <c.daniel.galmar@gmail.com>
2023-06-15 23:01:47 -0600

    agregando precios

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 const perfumesContainer = document.getElementById('perfumes-container');
 const modal = document.getElementById('modal');
 const modalImage = document.getElementById('modal-image');
+const modalImageQuotation = document.getElementById('modal-imageQuotation');
 const TituloPerfume = document.getElementById('TituloPerfume');
+const TituloPerfumeQuotation = document.getElementById('TituloPerfumeQuotation');
 const modalDescription = document.getElementById('modal-description');
 const modalClose = document.getElementById('modal-close');
+const cotizar = document.getElementById('btnCotizar');
+
 
 // Función para mostrar el modal con el perfume seleccionado
-function mostrarModal(src, descripcion, nombre) {
+function mostrarModal(src, descripcion, nombre, p) {
   TituloPerfume.innerHTML = nombre
+  TituloPerfumeQuotation.innerHTML = nombre
   modalImage.src = src;
+  modalImageQuotation.src = src;
   modalDescription.innerHTML = descripcion;
+  $(cotizar).data('pdp', p);
   $('#mdlParfumDetail').modal('show')
 }
 
@@ -19,8 +26,9 @@ perfumesContainer.addEventListener('click', e => {
   if (perfume) {
     const nombre = perfume.querySelector('.perfume-name').innerHTML;
     const image = perfume.querySelector('.perfume-image');
+    const p = perfume.querySelector('.perfume-p').attributes['ptv'].value
     const description = perfume.querySelector('.perfume-description').innerHTML;
-    mostrarModal(image.src, description, nombre);
+    mostrarModal(image.src, description, nombre, p);
   }
 });
 
@@ -40,3 +48,18 @@ searchInput.addEventListener('input', function() {
     } 
   }
 });
+
+cotizar.addEventListener('click', () => {
+  let pArr = $(cotizar).data('pdp').split('|');
+  let _p = parseInt(pArr[0]);
+  let _ml = parseInt(pArr[1])
+  let _ga = parseInt((Math.pow(6,3)) - 16);
+  let _g = 0.85;
+  let pb = parseInt(Math.round((_p + _ga)/_g))
+  let p = Math.round(pb/_ml)
+  $('#modal-pQuotation').text('Costo por mililitro: $' + p)
+  $('#mdlParfumQuotation').modal('show')
+  setTimeout(() => {
+    $('#mdlParfumDetail').modal('hide')
+  }, 300);
+})
