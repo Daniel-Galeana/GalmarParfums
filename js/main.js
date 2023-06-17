4d84a8cf11be73f7bfa30c805d6bf0bd8e5de83e
Daniel Galeana <c.daniel.galmar@gmail.com>
2023-06-17 17:18:03 -0600

    modificando el modal de la vista de decants

--- js/main.js
+++ js/main.js
@@ -6,18 +6,41 @@ const TituloPerfume = document.getElementById('TituloPerfume');
 const TituloPerfumeQuotation = document.getElementById('TituloPerfumeQuotation');
 const modalDescription = document.getElementById('modal-description');
 const modalClose = document.getElementById('modal-close');
-const cotizar = document.getElementById('btnCotizar');
 
+$("#demo02").animatedModal({
+  animatedIn:'lightSpeedIn',
+  animatedOut:'bounceOutDown',
+  color:'#fff',
+  // Callbacks
+  beforeOpen: function() {
+      console.log("The animation was called");
+  },           
+  afterOpen: function() {
+      console.log("The animation is completed");
+  }, 
+  beforeClose: function() {
+      console.log("The animation was called");
+  }, 
+  afterClose: function() {
+      console.log("The animation is completed");
+  }
+});
 
 // Función para mostrar el modal con el perfume seleccionado
-function mostrarModal(src, descripcion, nombre, p) {
+function mostrarModal(src, descripcion, nombre, p, seasson) {
+  let htmlElement = '<canvas id="grafica" width="300" height="400"></canvas>'
+  $('#grafica').remove()
+  $('#canvasContainer').append(htmlElement)
+  let jsonSeassons = parseJSON(seasson)
+  let c = cotizar(p);
+  $('#modal-pQuotation').text('$  ' + c)
   TituloPerfume.innerHTML = nombre
-  TituloPerfumeQuotation.innerHTML = nombre
   modalImage.src = src;
-  modalImageQuotation.src = src;
   modalDescription.innerHTML = descripcion;
-  $(cotizar).data('pdp', p);
-  $('#mdlParfumDetail').modal('show')
+  $('#demo02').click()
+  setTimeout(() => {
+    PintarGrafica(jsonSeassons)
+  }, 200);
 }
 
 // Event listeners
@@ -27,8 +50,9 @@ perfumesContainer.addEventListener('click', e => {
     const nombre = perfume.querySelector('.perfume-name').innerHTML;
     const image = perfume.querySelector('.perfume-image');
     const p = perfume.querySelector('.perfume-p').attributes['ptv'].value
+    const seasson = perfume.querySelector('.perfume-p').attributes['seassons'].value
     const description = perfume.querySelector('.perfume-description').innerHTML;
-    mostrarModal(image.src, description, nombre, p);
+    mostrarModal(image.src, description, nombre, p, seasson);
   }
 });
 
@@ -49,17 +73,59 @@ searchInput.addEventListener('input', function() {
   }
 });
 
-cotizar.addEventListener('click', () => {
-  let pArr = $(cotizar).data('pdp').split('|');
+const cotizar = (c) => {
+  let pArr = c.split('|');
   let _p = parseInt(pArr[0]);
   let _ml = parseInt(pArr[1])
   let _ga = parseInt((Math.pow(6,3)) - 16);
   let _g = 0.75;
   let pb = parseInt(Math.round((_p + _ga)/_g))
   let p = Math.round(pb/_ml)
-  $('#modal-pQuotation').text('Costo por mililitro: $' + p)
-  $('#mdlParfumQuotation').modal('show')
-  setTimeout(() => {
-    $('#mdlParfumDetail').modal('hide')
-  }, 300);
-})
+  return p
+}
+
+const parseJSON = (json) => {
+  let JSONformatter = json.replaceAll("'", '"');
+  return JSONformatter
+}
+
+const PintarGrafica = (json) => {
+  let perfume = JSON.parse(json);
+
+  // Obtener las temporadas y los porcentajes
+var temporadas = Object.keys(perfume);
+var porcentajes = Object.values(perfume);
+
+// Definir colores para cada temporada
+var colores = ['#00DFA2', '#DB005B', '#F79327', '#00C4FF'];
+
+// Crear la gráfica de pastel
+var ctx = document.getElementById('grafica').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: temporadas,
+    datasets: [{
+      label: 'Porcentaje de usabilidad ',
+      data: porcentajes,
+      backgroundColor: colores
+    }]
+  },
+  options: {
+    responsive: false, // Deshabilitar la respuesta automática al tamaño de la pantalla
+        maintainAspectRatio: false, // No mantener la relación de aspecto
+    plugins: {
+      legend: {
+        display: false, // Ocultar las leyendas
+        onClick: function() {}, // Evitar acción al hacer clic en la leyenda
+        onHover: function() {} // Evitar cambio de cursor al pasar sobre la leyenda
+      },
+      tooltip: {
+        filter: function() { // Evitar mostrar tooltips al pasar sobre los elementos de la gráfica
+          return true;
+        }
+      }
+    }
+  }
+});
+}
